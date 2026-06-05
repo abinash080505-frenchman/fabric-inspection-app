c6bbfc4134624a3a9eee6f46b79d0193fa735b56
abinash080505-frenchman <abinash080505@gmail.com>
2026-06-06 00:36:36 +0530

    Update app.py

--- Fabric-Inspection-App/app.py
+++ Fabric-Inspection-App/app.py
@@ -19,8 +19,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-
-  
+    image = image.convert("RGB")
 
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
