13c08ead01c678db2166369225a9c679ec7f89b4
abinash080505-frenchman <abinash080505@gmail.com>
2026-06-06 00:44:05 +0530

    Update app.py

--- Fabric-Inspection-App/app.py
+++ Fabric-Inspection-App/app.py
@@ -17,6 +17,8 @@ st.write("Upload a fabric image to detect defects.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     image = image.convert("RGB")
@@ -24,19 +26,13 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
-        image.save(tmp.name, format="JPEG")
+        image.save(tmp.name, "JPEG")
         results = model(tmp.name)
 
     annotated_image = results[0].plot()
 
     st.subheader("🔍 Detection Result")
     st.image(annotated_image, use_container_width=True)
-
-    if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    image = image.convert("RGB")
-
-    st.image(image, caption="Uploaded Image", use_container_width=True)
 
 
     # Temporary save for YOLO
